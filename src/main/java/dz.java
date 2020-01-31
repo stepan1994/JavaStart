577de8b62f36b6f9f022f4ed017cb1acde726660
Dmitriy <hyteded@gmail.com>
2020-01-31 14:05:25 +0200

    Initial commit: test

---
+++ src/main/java/dz.java
@@ -0,0 +1,24 @@
+public class dz {
+    public static void main(String [] args) {
+        int a = 7;
+        int b = 7;
+        if (b - a > 0) {
+            System.out.println("b bolshe a");
+        } else if (b - a < 0) {
+            System.out.println("b menshe a");
+        }
+
+        else {
+            System.out.println("a ravno b");
+        }
+
+        int c = -3;
+                if (c % 2 != 0) {
+            System.out.println("c - ne 4etnoe");
+        } else if (c % 2 == 0) {
+            System.out.println("c - 4etnoe");
+        }
+
+    }
+
+}
